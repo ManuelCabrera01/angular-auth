9254ae231aecface7973daf79a277a55c212a4f3
Manuel <m.a.c.p90@gmail.com>
2017-11-18 14:50:52 -0500

    modification on user-model

--- models/user-model.js
+++ models/user-model.js
@@ -1,15 +1,22 @@
 const moongoose = require ("mongoose");
 const Schema   = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
+const userSchema = new Schema(
+  {
+  username:{
+      typer:String,
+     require :[true "Username in require" ],
+}
+  password: {
+    type: String,
+    require :[true , "passwordids require"],
+
 }, {
   timestamps: {
     createdAt: 'created_at',
     updatedAt: 'updated_at'
   }
-});
-
+}});
+//"User -> "user"->"users" collection.................
 const User = mongoose.model('User', userSchema);
 module.exports = User;
